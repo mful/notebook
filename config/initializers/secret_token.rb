5af4f04c732937852fa46bd4b958c3d02404bb6a
Matt <fulton.matthew@gmail.com>
2014-08-15 07:42:27 -0400

    Move to using ENV vars for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Annotate::Application.config.secret_key_base = 'a0d50200c5a032d6fa21eadb0f5b82fa7c0cebb703a4c607a3ef6da94f14199d5584a7e35214b951532bd485cb39ce354935ebc364a1e57d4045bd34db4d2c90'
+Annotate::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
