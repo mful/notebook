2f7ffd8920ca392c9898e201f906434b755b212a
Matt <fulton.matthew@gmail.com>
2015-02-11 11:06:52 -0500

    Ensure annotation before trying to comment on it

--- app/controllers/paper/comments_controller.rb
+++ app/controllers/paper/comments_controller.rb
@@ -2,10 +2,18 @@ class Paper::CommentsController < ApplicationController
 
   layout 'paper'
 
+  before_filter :find_annotation, only: [:new]
+
   def new
     @presenter = {
       type: 'comment',
-      id: params[:id]
+      id: @annotation.id
     }
+  end
+
+  private
+
+  def find_annotation
+    @annotation = Annotation.find( params[:id] )
   end
 end
